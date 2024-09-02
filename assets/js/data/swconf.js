9b89cf292ffb2fba7239ff7a41e04ae4ef0d1c20
Yash Sarrof <yashsarrof@outlook.com>
2024-09-02 09:17:29 +0200

    updated site look

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,29 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1725259995',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/resume/',
+      
+        '/publications/',
+      
+        '/blog/',
+      
+
+      
+      
+        '/assets/js/dist/app.min.js',
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
