12a941368bac03db82b47389ab5cedbb79cf685d
Yash Sarrof <yashsarrof@outlook.com>
2026-03-24 11:58:41 +0100

    update sit with new page

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767720605',resources: [
+    cacheName: 'chirpy-1774349865',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
