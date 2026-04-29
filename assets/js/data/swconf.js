76cf3e4ee5380c25c7dc98e0c894c069711b9e46
Yash Sarrof <yashsarrof@outlook.com>
2026-04-29 17:17:14 +0200

    removed dark mode + added new publication

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1774349865',resources: [
+    cacheName: 'chirpy-1777475807',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
